2fd4dbb44e0da45055a807ec55db4b6b78477d77
Ekta Saini <34946048+ektasaini@users.noreply.github.com>
2020-08-25 13:18:12 +0530

    Update colors.h

--- colors.h
+++ colors.h
@@ -1,8 +1,10 @@
-#ifndef COLORS.H
-#define COLORS.H
-  
-    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-   
-  
-#endif 
+#ifndef COLORS_H
+#define COLORS_H
+
+namespace TelCoColorCoder
+{
+    enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
+    enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
+}
+
+#endif
